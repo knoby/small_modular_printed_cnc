3fd2bef16305ea2553a3d66c9459e0355ab27779
knoby <maximilian.brinkmann@posteo.de>
2021-05-04 08:33:32 +0200

    Implement Serial Recive and sending to Protocol handler

--- firmware/small-modular-printed-cnc/src/hw_config/serial.rs
+++ firmware/small-modular-printed-cnc/src/hw_config/serial.rs
@@ -8,6 +8,6 @@ type TxPin = stm32f1xx_hal::gpio::gpioa::PA2<Alternate<PushPull>>;
 type SerialDevice = stm32f1xx_hal::serial::Serial<stm32f1xx_hal::pac::USART2, (TxPin, RxPin)>;
 
 pub type SerialTx = stm32f1xx_hal::serial::Tx2;
-pub type SerialRx = stm32f1xx_hal::serial::RxDma2;
+pub type SerialRx = stm32f1xx_hal::serial::Rx2;
 
 type DmaChannel = stm32f1xx_hal::dma::dma1::C5;
--- firmware/small-modular-printed-cnc/src/main.rs
+++ firmware/small-modular-printed-cnc/src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 use defmt_rtt as _; // global logger
 use panic_probe as _;
 
@@ -13,31 +15,28 @@ mod misc;
 #[app(device = stm32f1xx_hal::device)]
 mod app {
 
-    use core::usize;
-
-    use cortex_m::singleton;
+    use defmt::debug;
     use stm32f1xx_hal::{prelude::_stm32_hal_flash_FlashExt, rcc::RccExt};
     use stm32f1xx_hal::{prelude::*, serial::Config};
-
-    use crate::hw_config::serial;
-    use nb::block;
 
     #[resources]
     struct Resources {
         #[task_local]
-        serial_tx: crate::hw_config::serial::SerialTx,
+        serial_tx: crate::hw_config::serial::SerialTx, // Sending Data over Serial
         #[task_local]
-        serial_rx: Option<
-            stm32f1xx_hal::dma::Transfer<
-                stm32f1xx_hal::dma::W,
-                &'static mut [u8; 128],
-                crate::hw_config::serial::SerialRx,
-            >,
-        >,
+        serial_rx: crate::hw_config::serial::SerialRx, // Reciving Data over Serial
+        #[task_local]
+        buffer_queue_in: heapless::spsc::Producer<'static, ([u8; 64], usize), 4>,
+        #[task_local]
+        buffer_queue_out: heapless::spsc::Consumer<'static, ([u8; 64], usize), 4>,
     }
 
     #[init]
     fn init(cx: init::Context) -> (init::LateResources, init::Monotonics) {
+        static mut SERIAL_QUEUE: heapless::spsc::Queue<([u8; 64], usize), 4> =
+            heapless::spsc::Queue::new();
+        let (buffer_queue_in, buffer_queue_out) = SERIAL_QUEUE.split();
+
         // Get Peripherals
         let device: stm32f1xx_hal::device::Peripherals = cx.device;
 
@@ -65,61 +64,76 @@ mod app {
             &mut rcc.apb1,
         );
 
-        let (mut serial_tx, serial_rx) = serial.split();
+        let (mut serial_tx, mut serial_rx) = serial.split();
 
-        let dma1_c6 = device.DMA1.split(&mut rcc.ahb).6;
+        // Enable the RX Interrupt
+        serial_rx.listen();
 
-        let mut serial_rx = serial_rx.with_dma(dma1_c6);
-
-        serial_rx.listen_idle_line();
-
-        let buffer = singleton!(:[u8; 128] = [0;128]).unwrap();
-
-        let serial_rx = serial_rx.read(buffer);
-
-        // Test Welcom Message
-        nb::block!(serial_tx.write(10)).ok();
+        // Send Welcome Message
         use core::fmt::Write;
-        serial_tx.write_str("Grbl 1.1h ['$' for help]").ok();
+        serial_tx.write_str("SMPCNC Version ").ok();
+        serial_tx.write_str(crate::VERSION).ok();
+        nb::block!(serial_tx.write(b'\n')).ok();
+        serial_tx.write_str("Grbl 1.1h ['$' for help]\n").ok();
 
         (
             init::LateResources {
-                serial_rx: Some(serial_rx),
+                serial_rx,
                 serial_tx,
+                buffer_queue_in,
+                buffer_queue_out,
             },
             init::Monotonics(),
         )
     }
 
-    #[task(binds = USART2, resources = [serial_rx, serial_tx])]
-    fn read_buffer(cx: read_buffer::Context) {
-        // Get Transfer
-        let serial_rx = cx.resources.serial_rx.take().unwrap();
-
-        // Stop Transfer and split in pices
-        let (buffer, mut serial_rx) = serial_rx.stop();
-        // Reset the Interrupt Event
-        serial_rx.reset_idle_line_event();
-
-        let bytes_rec = 128 - serial_rx.get_remaining_transfers();
-
-        defmt::debug!("Idle Line Detected Interrupt");
-        defmt::debug!("Recived {:?} bytes", bytes_rec);
-        if let Ok(text) = core::str::from_utf8(&buffer[..bytes_rec as usize]) {
-            defmt::debug!("Recived Text: {:?}", text);
-        } else {
-            defmt::debug!("No Valid String");
+    #[idle(resources = [buffer_queue_out, serial_tx])]
+    fn protocol_handler(cx: protocol_handler::Context) -> ! {
+        loop {
+            while let Some((buffer, length)) = cx.resources.buffer_queue_out.dequeue() {
+                if buffer[0] == b'$' && length >= 2 {
+                    debug!("System Command Recived");
+                } else {
+                    debug!("G-Code Recived");
+                }
+            }
         }
-
-        // Start Reciving again
-        let serial_rx = serial_rx.read(buffer);
-        cx.resources.serial_rx.replace(serial_rx);
     }
 
-    #[idle]
-    fn idle(_: idle::Context) -> ! {
-        loop {
-            cortex_m::asm::nop();
+    #[task(binds = USART2, resources = [serial_rx, buffer_queue_in])]
+    fn read_serial_buffer(cx: read_serial_buffer::Context) {
+        /// Serial Buffer for holding until a new line is detected
+        static mut SERIAL_BUFFER: heapless::Vec<u8, 64> = heapless::Vec::new();
+
+        while let Ok(data_byte) = cx.resources.serial_rx.read() {
+            match data_byte {
+                b'?' => {
+                    // Status Query
+                    debug!("Status Query recived");
+                }
+                b'~' => unimplemented!(), // Start/Resume
+                b'!' => unimplemented!(), // Feed Hold
+                0x18 => cortex_m::peripheral::SCB::sys_reset(), // Soft Reset
+                0x0A => {
+                    // Line Ended
+                    debug!("Line End recived");
+                    if SERIAL_BUFFER.len() > 0 {
+                        let mut data = [0; 64];
+                        for (place, data) in data.iter_mut().zip(SERIAL_BUFFER.iter()) {
+                            *place = *data;
+                        }
+                        cx.resources
+                            .buffer_queue_in
+                            .enqueue((data, SERIAL_BUFFER.len()))
+                            .ok();
+                        SERIAL_BUFFER.clear();
+                    }
+                }
+                _ => {
+                    // Normal Byte is Send to buffer
+                    SERIAL_BUFFER.push(data_byte).ok();
+                }
+            };
         }
     }
 }
